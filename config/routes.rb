df283c890397c1ffbd766eb9033b5f4b72de8b5e
Matt Hallowell <matthewhallowell1@gmail.com>
2017-06-03 18:53:34 -0500

    Created the home controller: Has the views for privacy, index, and about Manually created the files for login, logout, as user landing pages which will be implemented later.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root :to => 'home#index'
+  get 'home/index'
+  get 'home/about'
+  get 'home/privacy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
